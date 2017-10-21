1deeec8c467fa263ed59c0849e45e9816d7dacba
Michał Wąsowicz <mwasowicz7@gmail.com>
2017-10-21 16:29:42 +0200

    shorten main class name

--- src/test/java/com/iobestgroup/aplikacjadozarzadzaniawydatkamiswoimiinietylko/AppTests.java
+++ src/test/java/com/iobestgroup/aplikacjadozarzadzaniawydatkamiswoimiinietylko/AppTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AplikacjaDoZarzadzaniaWydatkamiSwoimiINieTylkoApplicationTests {
+public class AppTests {
 
 	@Test
 	public void contextLoads() {
