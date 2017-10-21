d5c4d00fb38ed83a6d9b7e5fbaf64f4e57c8736d
Michał Wąsowicz <mwasowicz7@gmail.com>
2017-10-21 15:05:02 +0200

    first commit

---
+++ src/main/java/com/iobestgroup/aplikacjadozarzadzaniawydatkamiswoimiinietylko/AplikacjaDoZarzadzaniaWydatkamiSwoimiINieTylkoApplication.java
@@ -0,0 +1,12 @@
+package com.iobestgroup.aplikacjadozarzadzaniawydatkamiswoimiinietylko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacjaDoZarzadzaniaWydatkamiSwoimiINieTylkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacjaDoZarzadzaniaWydatkamiSwoimiINieTylkoApplication.class, args);
+	}
+}
