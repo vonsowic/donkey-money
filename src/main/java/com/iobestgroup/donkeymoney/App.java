db1f61ef7a14c47326f65b83a52c769fa95f694e
Michał Wąsowicz <mwasowicz7@gmail.com>
2017-10-29 23:59:35 +0100

    add different database configuration

--- src/main/java/com/iobestgroup/donkeymoney/App.java
+++ src/main/java/com/iobestgroup/donkeymoney/App.java
@@ -2,8 +2,14 @@ package com.iobestgroup.donkeymoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:application-database.properties")
+})
 public class App {
 
 	public static void main(String[] args) {
