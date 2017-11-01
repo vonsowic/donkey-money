88026e3e3374f1ec10c47b6e0f8b3c0c9c7dc5f6
Michał Wąsowicz <mwasowicz7@gmail.com>
2017-11-01 16:38:12 +0100

    -

---
+++ src/main/java/com/iobestgroup/donkeymoney/database/PostgresConfig.java
@@ -0,0 +1,27 @@
+package com.iobestgroup.donkeymoney.database;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * @author miwas
+ * @version 1.0
+ * @since 01.11.17
+ */
+@ComponentScan
+@Configuration
+public class PostgresConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
