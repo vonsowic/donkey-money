45e7d176f0b6543fe097f09a91243fce48178a48
Michał Wąsowicz <mwasowicz7@gmail.com>
2017-11-23 22:54:41 +0100

    create empty methods in controllers

--- src/main/java/com/iobestgroup/donkeymoney/family/FamilyController.java
+++ src/main/java/com/iobestgroup/donkeymoney/family/FamilyController.java
@@ -1,12 +1,73 @@
 package com.iobestgroup.donkeymoney.family;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.iobestgroup.donkeymoney.security.SecurityConstants;
+import com.iobestgroup.donkeymoney.user.DMUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Michał Wąsowicz
  * @version 1.0
  * @since 12.11.17
  */
-@RestController("/api/family")
+@RestController
+@RequestMapping("/api/family")
 public class FamilyController {
+
+    private FamilyService familyDao;
+
+    @Autowired
+    public FamilyController(FamilyService familyDao) {
+        this.familyDao = familyDao;
+    }
+
+    @PostMapping("/create")
+    public void establishFamily(
+            @RequestHeader(SecurityConstants.HEADER_STRING) String token,
+            @RequestBody Family family){
+
+    }
+
+    @PostMapping("/add")
+    public void produceFamilyMember(
+            @RequestHeader(SecurityConstants.HEADER_STRING) String token,
+            @RequestBody DMUser user,
+            @RequestParam("id") Long familyId){
+
+    }
+
+    @GetMapping
+    public Family getMyFamily(
+            @RequestHeader(SecurityConstants.HEADER_STRING) String token,
+            @RequestParam("id") Long familyId){
+        return null;
+    }
+
+    @GetMapping("/all")
+    public Iterable<Family> getMyFamilies(
+            @RequestHeader(SecurityConstants.HEADER_STRING) String token){
+        return null;
+    }
+
+
+    @DeleteMapping
+    public void destroyFamily(
+            @RequestHeader(SecurityConstants.HEADER_STRING) String token,
+            @RequestParam("id") Long id){
+
+    }
+
+    @PutMapping
+    public void exileFamilyMember(
+            @RequestHeader(SecurityConstants.HEADER_STRING) String token,
+            @RequestParam("id") Long userId){
+
+    }
+
+    @DeleteMapping("/leave")
+    public void leaveFamily(
+            @RequestHeader(SecurityConstants.HEADER_STRING) String token,
+            @RequestParam("id") Long familyId){
+
+    }
 }
