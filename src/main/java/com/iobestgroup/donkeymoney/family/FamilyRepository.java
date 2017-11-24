082ea8f0b963f34dea5dd969241ef376badaa0e7
Michał Wąsowicz <mwasowicz7@gmail.com>
2017-11-24 16:25:04 +0100

    remove unnecessary method

--- src/main/java/com/iobestgroup/donkeymoney/family/FamilyRepository.java
+++ src/main/java/com/iobestgroup/donkeymoney/family/FamilyRepository.java
@@ -16,10 +16,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FamilyRepository extends CrudRepository<Family, Long>{
 
-    @Query("select f from families f where :family_id = id")
-    Family findFamilyById(@Param("family_id") Long familyId);
-
-
     @Query("select family from families family where lower(name) like :search")
     Iterable<Family> search(@Param("search") String search);
 
