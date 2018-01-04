db67c7a07e3669500bc7e7c365b0f0dd4a153787
Michał Wąsowicz <mwasowicz7@gmail.com>
2018-01-04 21:10:21 +0100

    remove cors

---
+++ src/main/java/com/iobestgroup/donkeymoney/security/WebSecurity.java
@@ -0,0 +1,21 @@
+package com.iobestgroup.donkeymoney.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+/**
+ * @author Michał Wąsowicz
+ * @version 1.0
+ * @since 12.11.17
+ */
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable();
+        http.csrf().disable();
+    }
+}
