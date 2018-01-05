cf1f647d8ac974d572c7cecaf5b5e1bf9d2da1de
Bartek Grodek <bartgrdk@gmail.com>
2018-01-05 13:13:42 +0100

    fix cors websecurity settings

--- src/main/java/com/iobestgroup/donkeymoney/security/WebSecurity.java
+++ src/main/java/com/iobestgroup/donkeymoney/security/WebSecurity.java
@@ -1,8 +1,12 @@
 package com.iobestgroup.donkeymoney.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 /**
@@ -15,9 +19,16 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.cors().ena();
+        http.cors().and();
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/**", "/*").permitAll();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
